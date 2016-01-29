ba79bbe08263da5ed53814a9487a8e9c2ac9f111
Eynard Thibault <contact@thibault-eynard.com>
2016-01-29 22:35:37 +0100

    First Commit

---
+++ UnityProject/Assets/Scripts/Book/Bubble.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public partial class Bubble : MonoBehaviour
+{
+    public static string bubblesPath = Application.streamingAssetsPath + "/Bubbles/";
+
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public Vector2 Size
+    {
+        get;
+        set;
+    }
+
+    public Bubble(string bubbleName)
+    {
+        BubbleInfo info = JsonUtility.FromJson<BubbleInfo>(File.ReadAllText(bubblesPath + bubbleName + ".json"));
+
+        this.Name = info.Name;
+        this.Size = new Vector2(info.Width, info.Height);
+    }
+
+    public class BubbleInfo
+    {
+        public string Name;
+        public float Width;
+        public float Height; 
+    }
+}
---
+++ UnityProject/Assets/Scripts/Book/Chapter.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Chapter : MonoBehaviour
+{
+    public static string chaptersPath = Application.streamingAssetsPath + "/Chapters/";
+
+    public Text ChapterTitleLabel;
+
+    private List<Bubble> bubbles = new List<Bubble>();
+
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public Chapter(string chapterName)
+    {
+        ChapterInfo info = JsonUtility.FromJson<ChapterInfo>(File.ReadAllText(chaptersPath + chapterName + ".json"));
+
+        string[] bubbleNames = info.Bubbles.Split(',');
+        for (int i = 0; i < bubbleNames.Length; ++i)
+        {
+            this.bubbles.Add(new Bubble(bubbleNames[i]));
+        }
+    }
+
+    public class ChapterInfo
+    {
+        public string Name;
+        public string Bubbles;
+    }
+}
