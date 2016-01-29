996111a4c9da94800c8544207a35a514d94ca3d3
Eynard Thibault <contact@thibault-eynard.com>
2016-01-29 22:57:51 +0100

    THE BOOK

---
+++ UnityProject/Assets/Scripts/Book/TheBook.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TheBook : MonoBehaviour
+{
+    private List<Chapter> ActiveChapters;
+
+    private void Awake()
+    {
+        this.ActiveChapters = new List<Chapter>();
+    }
+
+    private void Start ()
+    {
+	
+	}
+
+    public void StartStory()
+    {
+
+    }
+
+    public void StartDay()
+    {
+        this.ActiveChapters.Clear();        
+    }
+
+    public void AddChapter()
+    {
+
+    }
+}
