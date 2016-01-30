62ec47dc3453a04b16cb550779299a4fbf6ecc91
Eynard Thibault <contact@thibault-eynard.com>
2016-01-30 23:04:41 +0100

    FLASH CAMERA  ET SWAGG QUI MARCHE BIEN

--- UnityProject/Assets/Scripts/Book/TheBook.cs
+++ UnityProject/Assets/Scripts/Book/TheBook.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Text;
 using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System.Collections;
 
 public class TheBook : MonoBehaviour
 {
     public GameObject GuiPanel;
+    public CameraManager CameraManager;
     public Text InputText;
+    public GameObject InputField;
+    public Text StoryLabel;
+    public GameObject ScreenshotButton;
     public string[] KeyWords;
     public Sprite[] VignettesSprites;
     public SpriteRenderer[] Bubbles;
@@ -18,6 +24,7 @@ public class TheBook : MonoBehaviour
         
     private string currentInputText;
     private List<string> foundKeyWords;
+    private Vignette[] vignettes;
 
     private void Awake()
     {
@@ -26,33 +33,64 @@ public class TheBook : MonoBehaviour
 
     private void Start()
     {
-        
-	}
+        this.vignettes = this.VignetteContainer.GetComponentsInChildren<Vignette>();
+        this.ScreenshotButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            this.OnInputTextChange();
+        }
+    }
 
     public void OnInputTextChange()
     {
-        this.currentInputText = this.InputText.text;
+        this.ScreenshotButton.SetActive(false);
+        this.StoryLabel.text = this.InputText.text;
+        this.currentInputText = this.InputText.text.Replace(",", string.Empty);
         string[] words = this.currentInputText.Split(' ');
         this.foundKeyWords.Clear();
         
         for (int i = 0; i < words.Length; ++i)
         {
-            if (this.KeyWords.Contains(words[i]) && !this.foundKeyWords.Contains(words[i]))
+            string word = words[i].ToLower();
+            if (this.KeyWords.Contains(word) && !this.foundKeyWords.Contains(word))
             {
-                this.foundKeyWords.Add(words[i]);
+                this.foundKeyWords.Add(word);
             }
         }
 
-        for (int i = 0; i < this.Bubbles.Length; ++i)
+        if (this.foundKeyWords.Count <= 6)
         {
-            if (i < this.foundKeyWords.Count)
+            // Fill Right Bubbles
+            for (int i = 0; i < this.Bubbles.Length; ++i)
             {
-                int vignetteIndex = Array.IndexOf(this.KeyWords, this.foundKeyWords[i]);
-                this.Bubbles[i].sprite = this.VignettesSprites[vignetteIndex];
+                if (i < this.foundKeyWords.Count)
+                {
+                    int vignetteIndex = Array.IndexOf(this.KeyWords, this.foundKeyWords[i]);
+                    this.Bubbles[i].sprite = this.VignettesSprites[vignetteIndex];
+                }
+                else
+                {
+                    this.Bubbles[i].sprite = this.BlankBubble;
+                }
             }
-            else
+
+            //Disable left vignettes
+            for (int i = 0; i < this.vignettes.Length; ++i)
             {
-                this.Bubbles[i].sprite = this.BlankBubble;
+                this.vignettes[i].gameObject.SetActive(!this.foundKeyWords.Contains(this.vignettes[i].KeyWord));
+            }
+        }
+        
+        if (this.foundKeyWords.Count >= 6)
+        {
+            this.ScreenshotButton.SetActive(true);
+            foreach (Vignette vignette in this.vignettes)
+            {
+                vignette.gameObject.SetActive(false);
             }
         }
     }
@@ -72,5 +110,26 @@ public class TheBook : MonoBehaviour
     public void GameOver()
     {
         
+    }
+
+    public void TakeScreenshot()
+    {
+        this.StartCoroutine(this.TakeScreenshotCoroutine());
+    }
+
+    private IEnumerator TakeScreenshotCoroutine()
+    {
+        this.InputField.gameObject.SetActive(false);
+        this.ScreenshotButton.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(0.1f);
+
+        Application.CaptureScreenshot(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Boris.png");
+        this.StartCoroutine(this.CameraManager.FlashPhoto());
+
+        yield return new WaitForSeconds(0.8f);
+
+        this.InputField.gameObject.SetActive(true);
+        this.ScreenshotButton.gameObject.SetActive(true);
     }
 }
--- UnityProject/Assets/Scripts/Managers/CameraManager.cs
+++ UnityProject/Assets/Scripts/Managers/CameraManager.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class CameraManager : MonoBehaviour
 {
     public float FadeDuration = 60f;
+    public float FlashDuration = 60f;
+    public float MaxFlashIntensity = 3f;
 
     private UnityStandardAssets.ImageEffects.VignetteAndChromaticAberration Vignette
     {
         get
         {
             return this.GetComponent<UnityStandardAssets.ImageEffects.VignetteAndChromaticAberration>();
+        }
+    }
+
+    private UnityStandardAssets.ImageEffects.Bloom Bloom
+    {
+        get
+        {
+            return this.GetComponent<UnityStandardAssets.ImageEffects.Bloom>();
         }
     }
 
@@ -41,5 +51,32 @@ public class CameraManager : MonoBehaviour
         }
 
         this.Vignette.intensity = 1;
+    }
+
+    public IEnumerator FlashPhoto()
+    {
+        float counter = 0;
+        while (counter < this.FlashDuration)
+        {
+            counter++;
+            float progression = counter / this.FlashDuration;
+            this.Bloom.bloomIntensity = progression * this.MaxFlashIntensity;
+
+            yield return null;
+        }
+
+        this.Bloom.bloomIntensity = this.MaxFlashIntensity;
+
+        counter = 0;
+        while (counter < this.FlashDuration / 2)
+        {
+            counter++;
+            float progression = 1 - (counter / this.FlashDuration);
+            this.Bloom.bloomIntensity = progression * this.MaxFlashIntensity;
+
+            yield return null;
+        }
+
+        this.Bloom.bloomIntensity = 0;
     }
 }
